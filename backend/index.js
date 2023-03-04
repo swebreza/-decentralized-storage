390ebb0769f5e4c74eedb0d11021073bb8e5bed6
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2023-03-04 23:34:15 +0530

    Re-Structuring

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const app = express()
 
-const port = 8000
+const port = 8000 //Port Number
+
+app.get('/', (req, res) => {
+  return res.send('Hello There!!!!')
+})
+
 app.get('/login', (req, res) => {
   return res.send('Hello login')
 })
-app.get('/', (req, res) => {
-  return res.send('Hello There!!!')
-})
-app.get('/home', (req, res) => {
-  return res.send('Hello Home')
+
+app.get('/signout', (req, res) => {
+  return res.send('Hello sign out')
 })
 
 app.listen(port, () => {
-  console.log('Server is running....')
+  console.log(`Server is running on Port ${port}`)
 })
+
+// Get
+// Post
+// put
+// delete
