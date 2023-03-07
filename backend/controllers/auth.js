859be2213d628a4d6d65627f220f6827bc1cbd80
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2023-03-07 21:29:23 +0530

    User Validation Check

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,14 @@
+const { validationResult } = require('express-validator')
 const User = require('../modules/user')
 
 exports.signup = (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    })
+  }
+
   const user = new User(req.body)
   // return res.json(user)
   user
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const { check } = require('express-validator')
 const { signout, signup } = require('../controllers/auth')
 
-router.post('/signup', signup)
+router.post(
+  '/signup',
+  [
+    // username must be at least 3 chars long
+    check('name', 'Name Should be  at least 3 character').isLength({
+      min: 3,
+    }),
+    check('email', 'Email is required').isEmail(),
+    // password must be at least 3 chars long
+    check('password', 'Password should be at least 3 character ').isLength({
+      min: 3,
+    }),
+  ],
+  signup
+)
 router.get('/signout', signout)
 
 module.exports = router
