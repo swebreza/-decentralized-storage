05d1965f1d80a75d451ece65d477c0f8b678fe94
MD Suweb Reza <45898572+swebreza@users.noreply.github.com>
2023-03-21 08:43:17 +0530

    tailwind

--- backend/controllers/ipfs/ipfs.js
+++ backend/controllers/ipfs/ipfs.js
@@ -1,20 +1,12 @@
 const saveFile = require('./saveFile')
 const User = require('../../modules/user')
 
-exports.ipfsFile = async (req, res, id) => {
+exports.ipfsFile = async (req, res) => {
   // const id = req.profile._id
   const data = saveFile.saveFile()
   await data.then((result) => {
-    console.log(result)
-    User.findOne({ _id: id })
-      .populate('File')
-      .then((results) => {
-        res.json({
-          message: results,
-        })
-      })
-      .catch((err) => console.log(err))
-
-    return result
+    return res.json({
+      message: result,
+    })
   })
 }
